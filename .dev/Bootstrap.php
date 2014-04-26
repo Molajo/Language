c62cb0edb9b62cac57a1895e1b224c7adfaf38a7
AmyStephen <AmyStephen@gmail.com>
2014-04-26 14:07:47 -0500

    Packaging

--- .dev/Bootstrap.php
+++ .dev/Bootstrap.php
@@ -6,25 +6,13 @@
  * @copyright  2014 Amy Stephen. All rights reserved.
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  */
+$base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
+include $base . '/vendor/autoload.php';
 include_once __DIR__ . '/CreateClassMap.php';
 
-if (! defined('PHP_VERSION_ID')) {
-    $version = explode('.', phpversion());
-    define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
-}
-
-$base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
 $classmap = array();
-
 $results  = createClassMap($base . '/Source/Adapter/', 'Molajo\\Language\\Adapter\\');
 $classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Factories/', 'Molajo\\Factories\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/vendor/commonapi/language/', 'CommonApi\\Language\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/vendor/commonapi/exception/', 'CommonApi\\Exception\\');
-$classmap = array_merge($classmap, $results);
-
 $classmap['Molajo\\Language\\Driver'] = $base . '/Source/Driver.php';
 ksort($classmap);
 
